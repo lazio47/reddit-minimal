341f54cab6ab7f79b602e08554a50cd55539d524
Shelton Agostinho <sheltonagostinho@ua.pt>
2025-02-04 23:01:02 +0000

    Add - comments reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import subredditsReducer from "./slices/subredditsSlice";
 import postsReducer from "./slices/postsSlice";
+import commentsReducer from "./slices/commentsSlice";
 
 const store = configureStore({
     reducer: {
         subreddits: subredditsReducer,
         posts: postsReducer,
+        comments: commentsReducer
     }
 });
 
